46708c5d08549642fcd5dca5ed9f3cdea8b45675
ImaLi06 <imanollimache@gmail.com>
2026-07-03 00:10:00 -0500

    feat(routines): add sets and reps attributes to exercise-block entity

--- src/app/routines/domain/model/exercise-block.entity.ts
+++ src/app/routines/domain/model/exercise-block.entity.ts
@@ -11,21 +11,29 @@ export class ExerciseBlock implements BaseEntity {
   private _exerciseName: string;
   private _exerciseType: ExerciseType;
   private _order:        number;
+  private _sets:         number;
+  private _reps:         number;
 
   constructor(props: {
     id:           number;
     exerciseName: string;
     exerciseType: ExerciseType;
     order:        number;
+    sets:         number;
+    reps:         number;
   }) {
     this._id           = props.id;
     this._exerciseName = props.exerciseName;
     this._exerciseType = props.exerciseType;
     this._order        = props.order;
+    this._sets         = props.sets;
+    this._reps         = props.reps;
   }
 
   get id():           number       { return this._id; }
   get exerciseName(): string       { return this._exerciseName; }
   get exerciseType(): ExerciseType { return this._exerciseType; }
   get order():        number       { return this._order; }
+  get sets():         number       { return this._sets; }
+  get reps():         number       { return this._reps; }
 }
